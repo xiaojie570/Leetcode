f166c6ea374d86868e0240421a181dc2a9af3b1e
fuxiaojie <457732323@qq.com>
2019-04-12 20:27:09 +0800

    大写字母变为小写字母（签到题） beats 100%

---
+++ src/main/java/leetcode/string/easy/_709_To_Lower_Case.java
@@ -0,0 +1,22 @@
+package leetcode.string.easy;
+
+/**
+ * Created by lenovo on 2019/4/12.
+ * 将给定的字符串大写字母都变为小写字母（签到题）
+ */
+public class _709_To_Lower_Case {
+    public String toLowerCase(String str) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < str.length(); i++) {
+            char temp = str.charAt(i);
+            if(temp >= 'a' && temp <= 'z') {
+                sb.append(temp);
+            } else if(temp >= 'A' && temp <= 'Z'){
+                sb.append((char)(temp - 'A' + 'a'));
+            } else {
+                sb.append(temp);
+            }
+        }
+        return new String(sb);
+    }
+}
