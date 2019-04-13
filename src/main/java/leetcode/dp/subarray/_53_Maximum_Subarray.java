b4fef9b00fb0233548a916667b4b58ec25dfe8b3
fuxiaojie <457732323@qq.com>
2019-04-13 10:52:02 +0800

    连续子元素的最大的和 beats 100%

---
+++ src/main/java/leetcode/dp/subarray/_53_Maximum_Subarray.java
@@ -0,0 +1,23 @@
+package leetcode.dp.subarray;
+
+/**
+ * Created by lenovo on 2019/4/13.
+ * 给定一个字符数组，找到连续子数组和最大的和
+ * 【解题思路】
+ *  1. 如果和sum小于0，就把之前的和抛弃，变为当前元素的值
+ *  2. 如果和sum大于0，就加上当前元素的值作为新的和sum
+ *  3. 每次循环都更新返回值max的值，判断之前的max与新和sum大小，得到大的放到max中
+ */
+public class _53_Maximum_Subarray {
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE, sum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(sum < 0)
+                sum = nums[i];
+            else
+                sum += nums[i];
+            max = Math.max(max,sum);
+        }
+        return max;
+    }
+}
