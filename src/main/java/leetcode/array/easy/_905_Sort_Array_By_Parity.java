d62744a3f145725c09d7de5770ab305facbae50e
fuxiaojie <457732323@qq.com>
2019-04-13 12:06:14 +0800

    给定数组，奇数放在后边，偶数放到前面（签到题） beats 100%

---
+++ src/main/java/leetcode/array/easy/_905_Sort_Array_By_Parity.java
@@ -0,0 +1,24 @@
+package leetcode.array.easy;
+
+/**
+ * Created by lenovo on 2019/4/13.
+ * 给定数组，把奇数放到后边，偶数放到前边
+ * 【解题思路】
+ *      两个指针，签到题
+ */
+public class _905_Sort_Array_By_Parity {
+    public int[] sortArrayByParity(int[] A) {
+        int left = 0, right = A.length - 1;
+        while(left < right) {
+            while(left < A.length && A[left] % 2 == 0) left ++;
+            while(right >= 0 && A[right] % 2 != 0) right --;
+            if(left < right) {
+                int temp = A[left];
+                A[left] = A[right];
+                A[right] = temp;
+                left ++; right--;
+            }
+        }
+        return A;
+    }
+}
