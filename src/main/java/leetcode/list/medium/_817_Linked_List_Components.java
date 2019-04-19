ad81e711815b5ea168c5da2851f6b79d111fd45b
xiaojie570 <457732323@qq.com>
2019-04-19 22:59:12 +0800

    head: 0->1->2->3 G = [0, 1, 3] OutPut=2

---
+++ src/main/java/leetcode/list/medium/_817_Linked_List_Components.java
@@ -0,0 +1,33 @@
+package leetcode.list.medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Input:
+ * head: 0->1->2->3
+ * G = [0, 1, 3]
+ * Output: 2
+ * Explanation:
+ * 0 and 1 are connected, so [0, 1] and [3] are the two connected components.
+ */
+public class _817_Linked_List_Components {
+    public int numComponents(ListNode head, int[] G) {
+        Set<Integer> hashSet = new HashSet<Integer>();
+        for(int i : G) hashSet.add(i);
+        ListNode node = head;
+        int temp = 0,ret = 0;
+        while(head != null) {
+            while(head != null && hashSet.contains(head.val)) {
+                head = head.next;
+                temp ++;
+            }
+            if(temp > 0) {
+                ret ++;
+                temp = 0;
+            }
+            if(head != null) head = head.next;
+        }
+        return ret;
+    }
+}
