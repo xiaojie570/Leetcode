c1be40286804551377901181b35f88509de166d5
fuxiaojie <457732323@qq.com>
2019-04-22 15:21:50 +0800

    机器人走路，只能往下走或者往右走。问有多少种走法

---
+++ src/main/java/leetcode/dp/path_sum/_62_Unique_Paths.java
@@ -0,0 +1,21 @@
+package leetcode.dp.path_sum;
+
+/**
+ * Created by lenovo on 2019/4/22.
+ * 机器人走路，只能往下走或者往右走。问有多少种走法
+ */
+public class _62_Unique_Paths {
+    public int uniquePaths(int m, int n) {
+        int dp[][] = new int[m][n];
+        for(int i = 0 ; i < m; i++)
+            dp[i][0] = 1; // 每一行的第一列
+        for(int i = 0; i < n; i++)
+            dp[0][i] = 1; // 第一行
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j ++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
