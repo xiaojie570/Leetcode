8ebac29d28fe0b285906929f3bcfc4ae5bb72dd8
fuxiaojie <457732323@qq.com>
2019-04-30 15:10:32 +0800

    给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。

---
+++ src/main/java/leetcode/dp/medium/_120_Triangle.java
@@ -0,0 +1,22 @@
+package leetcode.dp.medium;
+
+import java.util.List;
+
+/**
+ * Created by lenovo on 2019/4/30.
+ * 给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。
+ */
+public class _120_Triangle {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
+            }
+        }
+
+        return dp[0];
+    }
+}
