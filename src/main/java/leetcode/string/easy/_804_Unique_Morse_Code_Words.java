ed484c63ba4f9e209a107b0b4a24c24b43227696
fuxiaojie <457732323@qq.com>
2019-05-07 10:05:17 +0800

    摩斯代码

---
+++ src/main/java/leetcode/string/easy/_804_Unique_Morse_Code_Words.java
@@ -0,0 +1,25 @@
+package leetcode.string.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by lenovo on 2019/5/7.
+ * 摩斯代码
+ */
+public class _804_Unique_Morse_Code_Words {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] MORSE = new String[]{".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+                "....", "..", ".---", "-.-", ".-..", "--", "-.",
+                "---", ".--.", "--.-", ".-.", "...", "-", "..-",
+                "...-", ".--", "-..-", "-.--", "--.."};
+        Set<String> set = new HashSet<String>();
+        for(String str: words) {
+            StringBuilder sb = new StringBuilder();
+            for(char c : str.toCharArray())
+                sb.append(MORSE[c - 'a']);
+            set.add(sb.toString());
+        }
+        return set.size();
+    }
+}
