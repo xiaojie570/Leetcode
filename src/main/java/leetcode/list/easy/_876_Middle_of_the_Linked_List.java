0d0889f8aba965342b350528a223e8595cb32a08
fuxiaojie <457732323@qq.com>
2019-05-29 17:15:19 +0800

    找到链表的中间元素

--- src/main/java/leetcode/list/easy/_876_Middle_of_the_Linked_List.java
+++ src/main/java/leetcode/list/easy/_876_Middle_of_the_Linked_List.java
@@ -1,10 +1,14 @@
-package leetcode.list;
+package leetcode.list.easy;
+
+import leetcode.list.*;
 
 /**
  * Created by lenovo on 2019/4/12.
  * 找到链表的中间元素
  * 【解题思路】
  *      快慢指针。
+ *
+ *     解析： https://blog.csdn.net/xiaojie_570/article/details/90674682
  */
 public class _876_Middle_of_the_Linked_List {
     public ListNode middleNode(ListNode head) {
