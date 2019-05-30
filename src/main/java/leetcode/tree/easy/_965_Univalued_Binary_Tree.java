129020ccb0ba1f6169c53858352dd96febb793d3
fuxiaojie <457732323@qq.com>
2019-05-30 09:58:53 +0800

    判断给定的二叉树是否是所有节点的元素值都相等

--- src/main/java/leetcode/tree/easy/_965_Univalued_Binary_Tree.java
+++ src/main/java/leetcode/tree/easy/_965_Univalued_Binary_Tree.java
@@ -8,6 +8,8 @@ import java.util.Set;
  * 判断给定的二叉树是否是所有节点的元素值都相等
  * 【解题思路】
  *      1. 首先因为是所有节点的元素值都相等，所以可以将根节点存储下来，然后传递给私有方法，去递归的判断根节点的左孩子和右孩子的值是否和val根节点的值相同
+ *
+ *    CSDN: https://blog.csdn.net/xiaojie_570/article/details/90691836
  */
 public class _965_Univalued_Binary_Tree {
     public boolean isUnivalTree(TreeNode root) {
