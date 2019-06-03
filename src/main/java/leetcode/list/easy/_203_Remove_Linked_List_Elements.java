06ca44023cab643b7f1f779396950b63aed29d10
fuxiaojie <457732323@qq.com>
2019-06-03 20:56:53 +0800

    给定一个链表和一个值，将链表中的给定值节点删除。

--- src/main/java/leetcode/list/easy/_203_Remove_Linked_List_Elements.java
+++ src/main/java/leetcode/list/easy/_203_Remove_Linked_List_Elements.java
@@ -2,6 +2,8 @@ package leetcode.list.easy;
 
 /**
  * Created by lenovo on 2019/4/12.
+ *
+ * 给定一个链表和一个值，将链表中的给定值节点删除。
  */
 public class _203_Remove_Linked_List_Elements {
     public ListNode removeElements(ListNode head, int val) {
