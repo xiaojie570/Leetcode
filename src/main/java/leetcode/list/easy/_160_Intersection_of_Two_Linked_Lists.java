d74db3081b51bf53c8a6c24fa3c494cd232f759c
fuxiaojie <457732323@qq.com>
2019-06-03 21:19:32 +0800

    找到两个链表中的重合的结点的第一个结点

--- src/main/java/leetcode/list/easy/_160_Intersection_of_Two_Linked_Lists.java
+++ src/main/java/leetcode/list/easy/_160_Intersection_of_Two_Linked_Lists.java
@@ -3,6 +3,13 @@ package leetcode.list.easy;
 /**
  * Created by lenovo on 2019/4/18.
  * 找到两个链表中的重合的结点的第一个结点
+ *
+ * 【解题思路】
+ * 一定是按照短的链表进行遍历的，所以需要将长的链表遍历到跟短的链表一样长再开始一起遍历
+
+ 步骤一： 首先得到两个链表的长度。
+ 步骤二： 遍历长的那个链表，使得长的链表的剩下的长度等于短的链表的长度
+ 步骤三： 判断两个链表是否相等，如果相等就直接返回，否则继续向下遍历
  */
 public class _160_Intersection_of_Two_Linked_Lists {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
