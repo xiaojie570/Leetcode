9ecb27cc333f45debdb0ef01e3ae6bb21b7e4a00
fuxiaojie <457732323@qq.com>
2019-06-03 20:07:55 +0800

    判断给定的链表是否有环

--- src/main/java/leetcode/list/easy/_141_Linked_List_Cycle.java
+++ src/main/java/leetcode/list/easy/_141_Linked_List_Cycle.java
@@ -5,6 +5,8 @@ package leetcode.list.easy;
  * 判断给定的链表是否有环
  * 【解题思路】 修改链表结构
  *      循环，把链表中遍历到的每个节点都指向自身，循环退出的条件就是如果当前节点的下一个节点是指向自己，那么就存在环
+ *
+ *      CSDN: https://blog.csdn.net/xiaojie_570/article/details/90758089
  */
 public class _141_Linked_List_Cycle {
     public boolean hasCycle(ListNode head) {
