4dcbdc29a475ecd2447dd8f6d42d53419248d182
fuxiaojie <457732323@qq.com>
2019-06-03 21:41:30 +0800

    多维树的前序遍历

--- src/main/java/leetcode/tree/easy/_589_N_ary_Tree_Preorder_Traversal.java
+++ src/main/java/leetcode/tree/easy/_589_N_ary_Tree_Preorder_Traversal.java
@@ -9,6 +9,8 @@ import java.util.List;
  * 【解题思路】
  *    1. 首先要判断给定的树是否是空树，如果是空树，就直接返回false
  *    2. 写一个方法，用来递归将树中的元素放到List中。
+ *
+ *    CSDN: https://blog.csdn.net/xiaojie_570/article/details/90759590
  */
 public class _589_N_ary_Tree_Preorder_Traversal {
     public List<Integer> preorder(Node root) {
