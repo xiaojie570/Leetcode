81f5653f087c86ce277176a6d536f834eabb5459
fuxiaojie <457732323@qq.com>
2019-06-11 16:00:34 +0800

    连续子元素的最大的乘积

--- src/main/java/leetcode/dp/subarray/_152_Maximum_Product_Subarray.java
+++ src/main/java/leetcode/dp/subarray/_152_Maximum_Product_Subarray.java
@@ -8,6 +8,9 @@ package leetcode.dp.subarray;
  *      2. 两次for循环，每一次里面都计算乘以当前元素的结果cur，max的值等于max值与cur的最大值
  *          2.1 如果cur == 0，说明当前值为0，那么就要抛弃之前乘以过的值，因为0乘以任何数都等于0，将cur重新置为 1
  *          2.2 两次for 循环的目的是防止遇到了 10 -4 155 的情况，从前面往后算的max为10 从后往前算的max 为 155。
+ *
+ *
+ *     CSDN: https://blog.csdn.net/xiaojie_570/article/details/91444029
  */
 public class _152_Maximum_Product_Subarray {
     public int maxProduct(int[] nums) {
