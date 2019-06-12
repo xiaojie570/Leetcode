1c332c81cc7ba74c3d4ef78cd3338ad4039bba77
fuxiaojie <457732323@qq.com>
2019-06-12 13:02:42 +0800

    degree为一个数组中的元素出现频率最大值。要求寻找这个数组中出现频率最大的元素的最小距离。

--- src/main/java/leetcode/array/easy/_697_Degree_of_an_Array.java
+++ src/main/java/leetcode/array/easy/_697_Degree_of_an_Array.java
@@ -2,21 +2,25 @@ package leetcode.array.easy;
 
 /**
  * Created by lenovo on 2019/5/27.
- * degree为一个数组中的元素出现频率最大值。要求寻找这个数组中出现频率最大的额元素的最小距离。
+ *
+ * 【阿里笔试】
+ * degree为一个数组中的元素出现频率最大值。要求寻找这个数组中出现频率最大的元素的最小距离。
+ *
+ * CSDN: https://blog.csdn.net/xiaojie_570/article/details/91500172
  */
 public class _697_Degree_of_an_Array {
     public int findShortestSubArray(int[] nums) {
         int[] head = new int[50001];
-        int[] degree = new int[50001];
+        int[] degree = new int[50001];  // degree存储的是这个元素出现的频率
 
         for(int i = 0; i < 50000; i++)
-            head[i] = -1;
+            head[i] = -1;  // 先定义一个head数组，赋初始值 -1
 
         int  min = nums.length, max = 1;
         for(int i = 0; i < nums.length; i++) {
-            if(head[nums[i]] == -1)
-                head[nums[i]] = i;
-            degree[nums[i]] ++;
+            if(head[nums[i]] == -1)  // 说明之前还没有元素
+                head[nums[i]] = i;  // head存储这个元素对应的下标位置，head的下标是每个元素nums[i]
+            degree[nums[i]] ++;  // degree存储的是这个元素出现的频率
             if(degree[nums[i]] > max) {
                 max = degree[nums[i]];
                 min = i - head[nums[i]] + 1;
