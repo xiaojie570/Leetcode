c8fe79a4820bc2310e2c9ed9c9496941428009b6
xiaojie570 <457732323@qq.com>
2019-06-12 22:31:59 +0800

    给定两个数组，交换两个数组中的一个数，使得两个数组中的和相等

--- src/main/java/leetcode/array/easy/_888_Fair_Candy_Swap.java
+++ src/main/java/leetcode/array/easy/_888_Fair_Candy_Swap.java
@@ -9,6 +9,8 @@ import java.util.Set;
  * 【解题思路】
  *      1. 首先分别计算出A和B数组的和，然后计算平均值 avg, 平均值就是A和B交换后数组中的和
  *      2. 然后依次遍历数组A，遍历到第 i 个数，如果把A[i]换掉，那么A中剩余元素的和是：sumA - A[i]。要想使A的各元素和达到avg，还差avg - (sumA - A[i])
+ *
+ *      CSDN: https://blog.csdn.net/xiaojie_570/article/details/91641877
  */
 public class _888_Fair_Candy_Swap {
     public int[] fairCandySwap(int[] A, int[] B) {
