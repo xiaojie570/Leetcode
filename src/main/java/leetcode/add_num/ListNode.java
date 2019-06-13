2905fc3d962a537abaee45454cafb0527ded5715
xiaojie570 <457732323@qq.com>
2019-06-13 16:55:19 +0800

    将两个链表中的元素相加，每一位只能存储一个0-9的数

---
+++ src/main/java/leetcode/add_num/ListNode.java
@@ -0,0 +1,13 @@
+package leetcode.add_num;
+
+/**
+ * Created by lenovo on 2019/4/12.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
