047b1c3b500dde6154c0a15248fbc79c494a0fa0
xiaojie570 <457732323@qq.com>
2019-06-13 17:32:20 +0800

    大数相加

---
+++ src/main/java/leetcode/add_num/_415_Add_Strings.java
@@ -0,0 +1,23 @@
+package leetcode.add_num;
+
+/**
+ * Created by lenovo on 2019/4/24.
+ * 大数相加
+ *
+ * CSDN: https://blog.csdn.net/xiaojie_570/article/details/91878048
+ */
+public class _415_Add_Strings {
+    public String addStrings(String num1, String num2) {
+        int length1 = num1.length() - 1, length2 = num2.length() - 1;
+        int temp = 0;
+        StringBuilder sb = new StringBuilder();
+        while(length1 >= 0 || length2 >= 0 || temp > 0) {
+            int a = length1 >= 0 ? num1.charAt(length1--) - '0' : 0;
+            int b = length2 >= 0 ? num2.charAt(length2--) - '0' : 0;
+            int left = ( a + b + temp) % 10;
+            temp = (a + b + temp) / 10;
+            sb.insert(0,String.valueOf((char)(left + '0')));
+        }
+        return sb.toString();
+    }
+}
