061f5110bd75c6d167efa034a39f711221138c3c
fuxiaojie <457732323@qq.com>
2019-06-14 16:23:44 +0800

    可以删除一个元素使得字符串是回文串

--- src/main/java/leetcode/string/easy/_680_Valid_Palindrome_II.java
+++ src/main/java/leetcode/string/easy/_680_Valid_Palindrome_II.java
@@ -3,6 +3,8 @@ package leetcode.string.easy;
 /**
  * Created by lenovo on 2019/5/7.
  * 给一个非空的字符串，你可以删除最多一个字符，判断之后这个字符串是否是回文串。
+ *
+ * CSDN: https://blog.csdn.net/xiaojie_570/article/details/91983225
  */
 public class _680_Valid_Palindrome_II {
     public boolean validPalindrome(String s) {
@@ -11,7 +13,7 @@ public class _680_Valid_Palindrome_II {
         return isValid(s,0,s.length() - 1,0,1);
     }
 
-    private boolean isValid(String s, int left, int right, int delCount, int N) {
+    private boolean isValid(String s, int left, int right, int delCount, int N) {  // delCount表示当前已经删除了多少个元素；N 表示可以删除的个数
         if(delCount > N)
             return false;
         while (left < right) {
