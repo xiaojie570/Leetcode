03ebf854d064634f4cb850d33c6199c7cd2ca386
xiaojie570 <457732323@qq.com>
2019-06-15 21:27:14 +0800

    找到数组中出现次数大于元素总数一半的元素值

--- src/main/java/leetcode/string/easy/_169_Majority_Element.java
+++ src/main/java/leetcode/string/easy/_169_Majority_Element.java
@@ -4,14 +4,17 @@ import java.util.Arrays;
 
 /**
  * Created by lenovo on 2019/5/8.
- * 找到数组中出现次数大院元素总数一半的元素值
+ * 找到数组中出现次数大于元素总数一半的元素值
  * 【解题思路】
  *      思路一：排序然后再找
  *      思路二：不需要排序，如果当前元素元素和前面的元素不一样，就将count--，如果一样就 count++。这个方法很好。。。
+ *      、
+ *
+ *   CSDN: https://blog.csdn.net/xiaojie_570/article/details/92197191
  */
 public class _169_Majority_Element {
     public int majorityElement(int[] nums) {
-        int cur_num =0;
+        int cur_num =0; // 当前的数
         int count =0;
 
         for(int num: nums){
