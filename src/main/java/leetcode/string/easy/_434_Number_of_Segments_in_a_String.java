21b167ee3b4f65564a643d1b857c679ee41a8900
xiaojie570 <457732323@qq.com>
2019-06-17 13:14:29 +0800

    返回给定字符串有多少个单词。单词是由空格分割的。

--- src/main/java/leetcode/string/easy/_434_Number_of_Segments_in_a_String.java
+++ src/main/java/leetcode/string/easy/_434_Number_of_Segments_in_a_String.java
@@ -2,6 +2,9 @@ package leetcode.string.easy;
 
 /**
  * Created by lenovo on 2019/5/7.
+ * 返回给定字符串有多少个单词。单词是由空格分割的。
+ *
+ * CSDN: https://blog.csdn.net/xiaojie_570/article/details/92606859
  */
 public class _434_Number_of_Segments_in_a_String {
     public int countSegments(String s) {
