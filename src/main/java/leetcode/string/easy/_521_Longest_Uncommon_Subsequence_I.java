71dff44ead3041a45b3e891a13e4cf3b83004089
xiaojie570 <457732323@qq.com>
2019-06-18 10:42:51 +0800

    给两个字符串，最长子序列是其中一个字符串的子序列而不是另一个字符串的子序列。

--- src/main/java/leetcode/string/easy/_521_Longest_Uncommon_Subsequence_I.java
+++ src/main/java/leetcode/string/easy/_521_Longest_Uncommon_Subsequence_I.java
@@ -5,6 +5,7 @@ package leetcode.string.easy;
  * 给两个字符串，最长子序列是其中一个字符串的子序列而不是另一个字符串的子序列。
  * 【解题思路】
  *  如果两个字符相同，就返回 -1， 如果不相同，则返回字符串长度大的那个字符串的长度即可
+ *  CSDN: https://blog.csdn.net/xiaojie_570/article/details/92767618
  */
 public class _521_Longest_Uncommon_Subsequence_I {
     public int findLUSlength(String a, String b) {
